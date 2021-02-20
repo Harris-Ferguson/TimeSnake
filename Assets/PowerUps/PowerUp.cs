cdf80660c50829eb58eec0c77a27b140c49ed6e4
Harris <haf094@usask.ca>
2021-02-20 11:50:08 -0600

    added full power up list to Ability enum

--- Assets/PowerUps/PowerUp.cs
+++ Assets/PowerUps/PowerUp.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum Ability
 {
-    NONE, REWIND, FREEZE, ASSGROW, BLUESHELL
+    NONE, REWIND, FREEZE, ASSGROW, BLUESHELL, FAST, SLOW
 }
 
 public class PowerUp : MonoBehaviour
