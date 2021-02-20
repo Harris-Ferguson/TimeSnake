e060bf8faadee09ea30e81cba2202ff325a267be
Harris <haf094@usask.ca>
2021-02-19 21:30:37 -0600

    added snake GameObject and movement controller

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public float gridSize;
+
+    private Vector2 moveDir;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MovePlayer", 0, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void HandleInput()
+    {
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            moveDir = Vector2.left;
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            moveDir = Vector2.right;
+        }
+        else if(Input.GetAxis("Vertical") > 0)
+        {
+            moveDir = Vector2.up;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            moveDir = Vector2.down;
+        }
+    }
+
+    public void MovePlayer()
+    {
+        transform.Translate(moveDir * gridSize);
+    }
+
+
+}
