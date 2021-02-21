ca1fc937768efb8293f81cb0a8fe04ab4e4487e1
Hamza Aziz <Hamza_975@hotmail.com>
2021-02-21 01:54:37 -0600

    Main Menu/ GUI work done

---
+++ Assets/MainMenu/MouseHover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    public Sprite sprite;
+    public Sprite highlightSprite;
+
+    void OnMouseOver()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = highlightSprite;
+    }
+
+    void OnMouseExit()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+}
